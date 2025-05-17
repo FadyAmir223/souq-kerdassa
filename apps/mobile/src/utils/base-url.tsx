57e43d931048dbaea547f9804b26994ccb7fa991
FadyAmir223 <fadyamir223@gmail.com>
2025-05-17 18:17:04 +0300

    build(mobile): fix prod crash

--- apps/mobile/src/utils/base-url.tsx
+++ apps/mobile/src/utils/base-url.tsx
@@ -1,11 +1,9 @@
 import Constants from 'expo-constants'
 
-import { env } from '@/lib/env'
-
 export const getBaseUrl = () => {
   const debuggerHost = Constants.expoConfig?.hostUri
   const localhost = debuggerHost?.split(':')[0]
 
-  if (!localhost) return env.EXPO_PUBLIC_SITE_URL
+  if (!localhost) return 'https://souqkerdassa.com' // env.EXPO_PUBLIC_API_URL
   return `http://${localhost}:3000`
 }
